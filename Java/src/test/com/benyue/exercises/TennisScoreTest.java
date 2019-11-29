d0ac0f013ab157abb75eb8c6ffe90066f5f7fbea
Ben <song.yue@microfocus.com>
2019-11-29 11:29:29 +0800

    Change name of the tennis score test

--- Java/src/test/com/benyue/exercises/TennisScoreTest.java
+++ Java/src/test/com/benyue/exercises/TennisScoreTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class tennisScoreTest{
+public class TennisScoreTest {
 
         String result ="";
         TennisScore tennisScore = new TennisScore();
