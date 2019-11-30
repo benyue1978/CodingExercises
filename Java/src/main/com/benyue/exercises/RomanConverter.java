9fc20c68f2ca2cb4b470457e97287972b4a85192
Ben Yue <song.yue@microfocus.com>
2019-11-30 17:00:34 +0800

    Add test 2

--- Java/src/main/com/benyue/exercises/RomanConverter.java
+++ Java/src/main/com/benyue/exercises/RomanConverter.java
@@ -1,10 +1,17 @@
 package com.benyue.exercises;
 
+import java.util.HashMap;
+
 public class RomanConverter {
+    HashMap<Integer, String> numberNumeralMap = new HashMap<>();
+
+    public RomanConverter() {
+        numberNumeralMap.put(1, "I");
+        numberNumeralMap.put(2, "II");
+        numberNumeralMap.put(5, "V");
+    }
+
     public String toRoman(int i) {
-        if (i==5){
-            return "V";
-        }
-        return "I";
+        return numberNumeralMap.get(i);
     }
 }
--- Java/src/test/com/benyue/exercises/RomanConverterTest.java
+++ Java/src/test/com/benyue/exercises/RomanConverterTest.java
@@ -6,14 +6,20 @@ import static org.junit.Assert.assertEquals;
 
 public class RomanConverterTest {
     @Test
-    public void OneTest() {
+    public void Test1() {
         RomanConverter converter = new RomanConverter();
         assertEquals("I", converter.toRoman(1));
     }
 
     @Test
-    public void FiveTest() {
+    public void Test5() {
         RomanConverter converter = new RomanConverter();
         assertEquals("V", converter.toRoman(5));
+    }
+
+    @Test
+    public void Test2() {
+        RomanConverter converter = new RomanConverter();
+        assertEquals("II", converter.toRoman(2));
     }
 }
