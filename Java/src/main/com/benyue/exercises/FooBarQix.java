833945d5b2813cdf27dea0d20c13effd9d4fb7bb
Ben Yue <song.yue@microfocus.com>
2019-12-14 08:58:55 +0800

    Add test two

--- Java/src/main/com/benyue/exercises/FooBarQix.java
+++ Java/src/main/com/benyue/exercises/FooBarQix.java
@@ -2,6 +2,6 @@ package com.benyue.exercises;
 
 public class FooBarQix {
     public String compute(int n) {
-        return "1";
+        return Integer.toString(n);
     }
 }
--- Java/src/test/com/benyue/exercises/FooBarQixTest.java
+++ Java/src/test/com/benyue/exercises/FooBarQixTest.java
@@ -5,9 +5,15 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FooBarQixTest {
+    FooBarQix fooBarQix = new FooBarQix();
+
     @Test
     public void One() {
-        FooBarQix fooBarQix = new FooBarQix();
         assertEquals("1", fooBarQix.compute(1));
+    }
+
+    @Test
+    public void Two() {
+        assertEquals("2", fooBarQix.compute(2));
     }
 }
