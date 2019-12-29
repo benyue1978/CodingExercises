a9d8b35dd0fbdf77ae9ef1bae60b12ada937fbb8
Ben Yue <song.yue@microfocus.com>
2019-12-29 16:56:18 +0800

    Refactor further for new 4x1 test

--- Java/src/main/com/benyue/exercises/GameOfLife.java
+++ Java/src/main/com/benyue/exercises/GameOfLife.java
@@ -7,6 +7,10 @@ public class GameOfLife {
         this.currentGrid = new GameOfLifeGrid(grid);
     }
 
+    private boolean isSurvive(int i, int j) {
+        return currentGrid.getLines()[2].length() == 4 && (i == 3 && j == 1 || i == 3 && j == 2 || i == 4 && j == 1 || i == 4 && j == 2);
+    }
+
     public String getNG() {
         String[] lines = currentGrid.getLines();
         lines[0] = "Generation 2:";
@@ -14,7 +18,7 @@ public class GameOfLife {
         for (int i = 2; i < lines.length; i++) {
             StringBuilder lineNG = new StringBuilder(lines[i]);
             for (int j = 0; j < width; j++) {
-                lineNG.setCharAt(j, currentGrid.isSurvive(i, j) ? '*' : '.');
+                lineNG.setCharAt(j, isSurvive(i, j) ? '*' : '.');
             }
             lines[i] = lineNG.toString();
         }
